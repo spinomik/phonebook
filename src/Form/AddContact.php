b04eb0425927c431cde4f4cb4e67ed62dbef12ab
spinomik <37461177+spinomik@users.noreply.github.com>
2021-06-21 00:16:32 +0200

    edycja addContact
    Dodanie do bazy danych tabeli Locality oraz do formularza pole dropdown wybierające miejscowości z bazy dancyh

--- src/Form/AddContact.php
+++ src/Form/AddContact.php
@@ -6,6 +6,8 @@ namespace App\Form;
 
 
 use App\Entity\Contact;
+use App\Entity\Locality;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\LocaleType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -21,7 +23,10 @@ class AddContact extends AbstractType
            ->add('Name')
            ->add('last_name')
            ->add('phone_number',TelType::class)
-           ->add('locality', LocaleType::class)
+           ->add('locality', EntityType::class,[
+               'class' => Locality::class,'choice_label' => function(Locality $locality){
+               return sprintf($locality->getName());
+                   },'placeholder'=>'Wybierz miejscowość'])
            ->add('zip_code')
            ->add('street')
            ->add('number_of_the_bulding',NumberType::class)
